ed846fa00e6265c62de7ae1106e7dd90388cf210
karina-talibzhanova <karina.talibzhanova@gmail.com>
2021-09-30 16:32:21 +0100

    some tests written for bubblesort

---
+++ Sort Manager/src/test/java/com/sparta/sort/manager/test/model/BubbleSortShould.java
@@ -0,0 +1,56 @@
+package com.sparta.sort.manager.test.model;
+
+import com.sparta.sort.manager.model.BubbleSort;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class BubbleSortShould {
+    static BubbleSort bubbleSorter;
+    @BeforeAll
+    public static void setUp() {
+        bubbleSorter = new BubbleSort();  // create BubbleSort object for all tests to use
+    }
+    @Test
+    public void getUnsortedArrayOfUniqueIntegers_ReturnSortedArray() {
+        int[] unsorted = {3, 4, 1, 2, 5};
+        int[] sorted = {1, 2, 3, 4, 5};
+        assertArrayEquals(sorted, bubbleSorter.sort(unsorted));
+    }
+
+    @Test
+    public void getUnsortedArrayOfNonUniqueIntegers_ReturnSortedArray() {
+        int[] unsorted = {3, 3, 1, 4, 2, 1, 5};
+        int[] sorted = {1, 1, 2, 3, 3, 4, 5};
+
+        assertArrayEquals(sorted, bubbleSorter.sort(unsorted));
+    }
+
+    @Test
+    public void getArrayLengthOne_ReturnArrayLengthOne() {
+        int[] target = {1};
+        assertArrayEquals(target, bubbleSorter.sort(target));
+    }
+
+    @Test
+    public void getArrayLengthZero_ReturnArrayLengthZero() {
+        int[] target = new int[0];
+        assertArrayEquals(target, bubbleSorter.sort(target));
+    }
+
+    @Test
+    public void getArrayNegativeIntegers_ReturnSortedArray() {
+        int[] unsorted = {-4, -1, -10, -3, -2};
+        int[] sorted = {-10, -4, -3, -2, -1};
+
+        assertArrayEquals(sorted, bubbleSorter.sort(unsorted));
+    }
+
+    @Test
+    public void getArrayMixedPositiveNegative_ReturnSortedArray() {
+        int[] unsorted = {-4, 0, 34, -5, -100, 32, 0};
+        int[] sorted = {-100, -5, -4, 0, 0, 32, 34};
+
+        assertArrayEquals(sorted, bubbleSorter.sort(unsorted));
+    }
+}
