ebef5f8924329d4e332cb44c7950f8c44e921b04
karina-talibzhanova <karina.talibzhanova@gmail.com>
2021-09-30 11:46:38 +0100

    refactored MergeSort so initial sorting method is called in public sort method

--- Sort Manager/src/com/sparta/sort/manager/model/MergeSort.java
+++ Sort Manager/src/com/sparta/sort/manager/model/MergeSort.java
@@ -4,11 +4,6 @@ import java.util.Arrays;
 
 public class MergeSort implements Sortable {
     public int[] sort(int[] arr) {
-        arr = mergeSort(arr);
-        return arr;
-    }
-
-    private int[] mergeSort(int[] arr) {
         if (arr.length <= 1) {
             return arr;
         }
@@ -19,10 +14,11 @@ public class MergeSort implements Sortable {
         int[] left = Arrays.copyOfRange(arr, 0, middle);
         int[] right = Arrays.copyOfRange(arr, middle, arr.length);
 
-        int[] leftSorted = mergeSort(left);
-        int[] rightSorted = mergeSort(right);
+        int[] leftSorted = sort(left);
+        int[] rightSorted = sort(right);
 
         return merge(leftSorted, rightSorted);
+
     }
 
     private int[] merge(int[] leftArr, int[] rightArr) {
@@ -57,43 +53,3 @@ public class MergeSort implements Sortable {
         return result;
     }
 }
-
-//        def merge(left, right):
-//        result = []
-//
-//        i = 0
-//        j = 0
-//
-//        while True:
-//        if left[i] > right[j]:
-//        result.append(left[i])
-//        i += 1
-//        else:
-//        result.append(right[j])
-//        j += 1
-//
-//        if i >= len(left) or j >= len(right):
-//        break
-//
-//        if i < len(left):
-//        result.extend(left[i:])
-//        elif j < len(right):
-//        result.extend(right[j:])
-//
-//        return result
-//
-//
-//        def hybrid_sort(arr):
-//        if len(arr) <= 4:
-//        return selection_sort(arr)
-//
-//        middle = len(arr) // 2
-//
-//        left = arr[:middle]
-//        right = arr[middle:]
-//
-//        left_sorted = hybrid_sort(left)
-//        right_sorted = hybrid_sort(right)
-//
-//        return merge(left_sorted, right_sorted)
-//
